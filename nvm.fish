62f9dad1628f4455ce6add01b01a790d2bfb0ba0
Félix Saparelli <felix@passcod.name>
2015-02-08 00:40:53 +1300

    Add functions

--- nvm.fish
+++ nvm.fish
@@ -1,18 +1,36 @@
+function nvm_set
+  echo set: $argv
+  set -gx $argv
+end
+
+function nvm_split_env
+  set k (echo $argv | cut -d\= -f1)
+  set v (echo $argv | cut -d\= -f2-)
+  echo "$k" "$v"
+end
+
+function nvm_split_path
+  echo $argv | grep -oE '[^:]+'
+end
+
 function nvm
   set nenv (env -i bash -c "source ~/.nvm/nvm.sh && nvm $argv 2>&1 >/dev/null; env")
   for e in $nenv
+    set k (echo $e | cut -d\= -f1)
+
     if test (echo $e | cut -d_ -f1) = NVM
-      set k (echo $e | cut -d\= -f1)
       set v (echo $e | cut -d\= -f2-)
-      set -gx $k "$v"
+      nvm_set $k $v
     else if test (echo $e | cut -d\= -f1) = PATH
-      set v (echo $e | cut -d\= -f2-)
-      set p (echo $v | grep -oE '[^:]+' | grep -w '.nvm' | head -n1)
-      set -gx PATH "$p" PATH
+      set o (echo $e | cut -d\= -f2-)
+      set p (echo $o | grep -oE '[^:]+' | grep -w '.nvm' | head -n1)
+      set v $p $PATH
+      nvm_set $k $v
     else if test (echo $e | cut -d\= -f1) = MANPATH
-      set v (echo $e | cut -d\= -f2-)
-      set p (echo $v | grep -oE '[^:]+' | grep -w '.nvm' | head -n1)
-      set -gx MANPATH "$p" MANPATH
+      set o (echo $e | cut -d\= -f2-)
+      set p (echo $o | grep -oE '[^:]+' | grep -w '.nvm' | head -n1)
+      set v $p $MANPATH
+      nvm_set $k $v
     end
   end
 end
